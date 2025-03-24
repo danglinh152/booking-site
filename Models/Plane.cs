a8649691805f650c6473d4ff816f0377a113be38
ledat139 <tiendatcntt1307@gmail.com>
2025-03-24 20:56:24 +0700

    add ignore

--- Models/Plane.cs
+++
@@ -1,9 +0,0 @@
-namespace BookingSite.Models
-{
-    public class Plane
-    {
-        public int AircraftID { get; set; }
-        public string Model { get; set; }
-        public int Capacity { get; set; }
-    }
-}
