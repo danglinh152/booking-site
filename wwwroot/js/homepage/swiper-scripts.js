bb64365b1a04f1fb846c145fab3ea50575c29c9b
danglinh152 <danglinh.k4@gmail.com>
2025-04-16 12:26:11 +0700

    client UI

--- wwwroot/js/homepage/swiper-scripts.js
+++ wwwroot/js/homepage/swiper-scripts.js
@@ -1,21 +1,25 @@
-var TrandingSlider = new Swiper('.tranding-slider', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    loop: true,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 100,
-        modifier: 2.5,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    }
+var TrandingSlider = new Swiper(".tranding-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 1000, // Thay đổi thời gian chuyển trang (3 giây)
+    disableOnInteraction: false, // Cho phép tự động chuyển tiếp khi người dùng tương tác
+  },
 });
